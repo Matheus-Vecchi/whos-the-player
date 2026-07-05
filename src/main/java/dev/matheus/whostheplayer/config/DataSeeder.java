0709b004c2d01f0b8b9e2f982307bb709d725dc8
Matheus-Vecchi <matheusvecchi02@gmail.com>
2026-07-04 21:57:24 -0300

    feat: seed daily players on startup

--- src/main/java/dev/matheus/whostheplayer/config/DataSeeder.java
+++ src/main/java/dev/matheus/whostheplayer/config/DataSeeder.java
@@ -1,19 +1,31 @@
 package dev.matheus.whostheplayer.config;
 
+import dev.matheus.whostheplayer.entity.DailyPlayer;
 import dev.matheus.whostheplayer.entity.Player;
 import dev.matheus.whostheplayer.enums.Position;
+import dev.matheus.whostheplayer.repository.DailyPlayerRepository;
 import dev.matheus.whostheplayer.repository.PlayerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class DataSeeder implements CommandLineRunner {
     private final PlayerRepository playerRepository;
+    private final DailyPlayerRepository dailyPlayerRepository;
+    private final Clock gameClock;
 
-    public DataSeeder(PlayerRepository playerRepository) {
+    public DataSeeder(PlayerRepository playerRepository, DailyPlayerRepository dailyPlayerRepository, Clock gameClock) {
         this.playerRepository = playerRepository;
+        this.dailyPlayerRepository = dailyPlayerRepository;
+        this.gameClock = gameClock;
     }
 
     @Override
@@ -90,6 +102,20 @@ public class DataSeeder implements CommandLineRunner {
                 new Player("Benjamin Sesko", 22, "Slovenia", "Premier League", "Manchester United", Position.ATTACKER, 84),
                 new Player("Michael Olise", 24, "France", "Bundesliga", "Bayern Munich", Position.ATTACKER, 87)
             ));
+        }
+        if (dailyPlayerRepository.count() == 0) {
+            List<Player> players = playerRepository.findAll();
+            Collections.shuffle(players, new Random(42));
+
+            LocalDate start = LocalDate.now(gameClock);
+            LocalDate date = start;
+            List<DailyPlayer> dailyPlayerList = new ArrayList<>();
+            for (int i = 0; i < players.size(); i++) {
+                DailyPlayer dailyPlayer = new DailyPlayer(date, players.get(i));
+                dailyPlayerList.add(dailyPlayer);
+                date = date.plusDays(1);
+            }
+            dailyPlayerRepository.saveAll(dailyPlayerList);
         }
     }
 }
